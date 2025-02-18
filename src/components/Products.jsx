4469aaaaeb62131cc223dcbe5be901049305b5a1
Rohit <rohit.rajan031101@gmail.com>
2025-02-18 17:36:35 +0530

    autoheal

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -112,7 +112,7 @@ const Products = () => {
             <div
               id={product.id}
               key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+              className="col-md-4 col-sm-6 col-xs-8"
             >
               <div className="card text-center h-100" key={product.id}>
                 <img
