231560ca0b769cbce14b0736d880534b01019d8d
Rohit <rohit.rajan031101@gmail.com>
2025-02-19 13:02:31 +0530

    autoheal

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -121,28 +121,28 @@ const Products = () => {
                   alt="Card"
                   height={300}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">
+                <div>
+                  <h5 className="bard-title">
                     {product.title.substring(0, 12)}...
                   </h5>
-                  <p className="card-text">
+                  <p className="cat-text">
                     {product.description.substring(0, 90)}...
                   </p>
                 </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
+                <ul className="list-group">
+                  <li>$ {product.price}</li>
                   {/* <li className="list-group-item">Dapibus ac facilisis in</li>
                     <li className="list-group-item">Vestibulum at eros</li> */}
                 </ul>
                 <div className="card-body">
                   <Link
                     to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
+                    className="btn-dark m-1"
                   >
                     Buy Now
                   </Link>
                   <button
-                    className="btn btn-dark m-1"
+                    className="btn m-1"
                     onClick={() => {
                       toast.success("Added to cart");
                       addProduct(product);
@@ -167,7 +167,7 @@ const Products = () => {
             <hr />
           </div>
         </div>
-        <div className="row justify-content-center">
+        <div className="row">
           {loading ? <Loading /> : <ShowProducts />}
         </div>
       </div>
